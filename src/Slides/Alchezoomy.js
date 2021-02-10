97aa393ff72aad271ecd116c6fcc4a3d620de76c
Sjaan Hydrick <sjaanahydrick@gmail.com>
2021-02-10 11:57:15 -0800

    files made, background chosen

---
+++ src/Slides/Alchezoomy.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Alchezoomy extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
---
+++ src/Slides/MVB.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MVB extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
