9209c309b785591112199377cf044042b8a46336
Sjaan Hydrick <sjaanahydrick@gmail.com>
2021-02-10 19:25:39 -0800

    FullPage skeleton filled

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import FullPage from '../src/FullPage/FullPage.js'
 import './style/App.css';
 
 export default class App extends Component {
+
   render() {
     return (
-      <div className="App">
+      <div>
         <FullPage />
       </div>
     );
--- src/FullPage/FullPage.js
+++ src/FullPage/FullPage.js
@@ -1,10 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Alchezoomy from '../Slides/Alchezoomy';
+import Bio from '../Slides/Bio';
+import Landing from '../Slides/Landing';
+import MVB from '../Slides/MVB';
+import Petreon from '../Slides/Petreon';
+import Resume from '../Slides/Resume';
+import Skills from '../Slides/Skills';
 
 export default class FullPage extends Component {
     render() {
         return (
             <div>
-                
+                <Landing />
+                <Bio />
+                <Skills />
+                <MVB />
+                <Alchezoomy />
+                <Petreon />
+                <Resume />
             </div>
         )
     }
